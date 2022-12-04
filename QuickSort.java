332c94b86e7c1a89bda231fc59b7102d396f1114
jin-Rh <119802458+jin-Rh@users.noreply.github.com>
2022-12-04 17:16:31 +0100

    Quick Sort

--- QuickSort.java
+++ QuickSort.java
@@ -40,6 +40,6 @@ public class QuickSort {
         int temp = array[i + 1];
         array[i + 1] = array[high];
         array[high] = temp;
-        return i + 1;
+        return i + 1; 
     }
 }
